877d34ee68519a32372b1357b5c649364af575f4
Emre <bayrakemre89@gmail.com>
2020-04-23 14:46:24 +0100

    routes created | mongoose installed

--- routes/posts.js
+++ routes/posts.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/:id', function(req, res, next) {
+  res.send('respond with post by ID :' + req.params.id);
+});
+
+router.post('/', function(req, res, next) {
+  res.send('respond with added post');
+});
+
 module.exports = router;
